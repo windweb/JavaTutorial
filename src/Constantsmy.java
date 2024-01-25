1928479a01682947beefdeb05ecc58f856d47dd0
Aidarov Kirill <github@aidarov.ru>
2024-01-25 19:15:40 +0300

    init Constantsmy

--- src/Constantsmy.java
+++ src/Constantsmy.java
@@ -1,7 +1,7 @@
 public class Constantsmy {
     public static void main(String[] args) {
         final float pi = 3.14F; // константа обязательно должна быть инициализирована словом final
-        pi = 1; // ошибка: константа не может быть переопределена
+//        pi = 1; // ошибка: константа не может быть переопределена
         System.out.println(pi);
 
     }
