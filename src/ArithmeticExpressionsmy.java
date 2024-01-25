22dee8ba0e0d8d27c7f09d970491c16c42181caf
Aidarov Kirill <github@aidarov.ru>
2024-01-25 19:29:12 +0300

    init ArithmeticExpressionsmy

---
+++ src/ArithmeticExpressionsmy.java
@@ -0,0 +1,18 @@
+public class ArithmeticExpressionsmy {
+    public static void main(String[] args) {
+        int result1 = 10 / 3;
+        System.out.println(result1); // result = 3
+
+        double result2 = (double) 10 / (double) 3;
+        System.out.println(result2); // result2 = 3.3333333333333335
+
+        int x = 1;
+        x++; // x = x + 1
+//        ++x; // x = x + 1
+        System.out.println(x); // x = 2
+
+        int y = x++;
+        System.out.println(y);
+
+    }
+}
