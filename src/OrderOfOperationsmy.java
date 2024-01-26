412a72decd713d7de4f11eae1632eadd47b5aa72
Aidarov Kirill <github@aidarov.ru>
2024-01-26 16:46:27 +0300

    init OrderOfOperationsmy

---
+++ src/OrderOfOperationsmy.java
@@ -0,0 +1,15 @@
+public class OrderOfOperationsmy {
+    public static void main(String[] args) {
+        int x = 10 + 3 * 2;
+        System.out.println(x);
+        int z = (10 + 3) * 2;
+        System.out.println(z);
+/*
+        // Order of Operations is from left to right / Порядок действий слева направо
+        // () has the highest precedence / () имеют наивысший приоритет
+        * / has the next highest precedence / * / имеют следующий наивысший приоритет
+        // + - has the lowest precedence / + - имеют наименьший приоритет
+*/
+
+    }
+}
