6183df5b382244027b41d5a2ce2ed06473683761
Aidarov Kirill <github@aidarov.ru>
2024-01-27 23:51:48 +0300

    update F013_FormattingNumbers

--- src/F013_FormattingNumbers.java
+++ src/F013_FormattingNumbers.java
@@ -1,5 +1,18 @@
+import java.text.NumberFormat;
+
 public class F013_FormattingNumbers {
     public static void main(String[] args) {
+        // 0.1 или 10%
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format(1234567.891);
+        System.out.println(result);
 
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        String result2 = percent.format(0.1);
+        System.out.println(result2);
+        // или 
+        String result3 = NumberFormat.getPercentInstance().format(0.1);
+        System.out.println(result3);
+        
     }
 }
