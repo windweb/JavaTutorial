bac478c983ad43d88ad9b95951de6c680c67e773
Aidarov Kirill <github@aidarov.ru>
2024-01-29 15:26:51 +0300

    update F019_ComparisonOperators

--- src/F019_ComparisonOperators.java
+++ src/F019_ComparisonOperators.java
@@ -1,5 +1,16 @@
+// Comparison Operators / Операторы сравнения
+
 public class F019_ComparisonOperators {
     public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+        // cравним числа
+          System.out.println(x < y);
+          System.out.println(x > y);
+          System.out.println(x <= y);
+          System.out.println(x >= y);
+          System.out.println(x == y);
+          System.out.println(x != y);
 
     }
 }
