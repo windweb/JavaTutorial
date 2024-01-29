d1a43599baff5d58be6f7576d8393533b6ade6bc
Aidarov Kirill <github@aidarov.ru>
2024-01-29 15:26:28 +0300

    update F018_ControlFlow

--- src/F018_ControlFlow.java
+++ src/F018_ControlFlow.java
@@ -1,3 +1,9 @@
+/*
+Comparison Operators / Операторы сравнения
+Logical Operators / Логические операторы
+Conditional Statements / Условные операторы if
+Loops / Циклы
+*/
 public class F018_ControlFlow {
     public static void main(String[] args) {
 
