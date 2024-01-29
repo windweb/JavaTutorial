4eec8efcf00e479da5c6431ef88a90f42aecc2a5
Aidarov Kirill <github@aidarov.ru>
2024-01-29 17:37:21 +0300

    update F024_SwitchStatements

--- src/F024_SwitchStatements.java
+++ src/F024_SwitchStatements.java
@@ -1,5 +1,46 @@
+/* Switch Statements / Оператор switch -
+переключатель, который позволяет выбрать между несколькими вариантами.
+Пример:
+
+int role = 2;
+
+switch (role) {
+    case 1:
+        // do something
+        break;
+     case 2:
+        // do something
+        break;
+     default:
+        // do something
+}
+* */
+
 public class F024_SwitchStatements {
     public static void main(String[] args) {
+        String role = "admin";
+
+//        if (role == "admin")
+//            System.out.println("Hello, admin!");
+//        else if (role == "user")
+//            System.out.println("Hello, user!");
+//        else if (role == "guest")
+//            System.out.println("Hello, guest!");
+
+        // или
+        switch (role) {
+            case "admin":
+                System.out.println("Hello, admin!");
+                break;
+
+            case "user":
+                System.out.println("Hello, user!");
+                break;
+
+            default: // role == "guest":
+                System.out.println("Hello, guest!");
+        }
+
 
     }
 }
