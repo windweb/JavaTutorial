120c2b865f2ddebc31b66110edae3a432e5a579a
Aidarov Kirill <github@aidarov.ru>
2024-01-29 18:22:07 +0300

    update F027_WhileLoops

--- src/F027_WhileLoops.java
+++ src/F027_WhileLoops.java
@@ -1,5 +1,37 @@
+/* While loops / Цикл while
+while - цикл, который выполняет определенное количество итераций до тех пор,
+        пока пока выражение истинно.
+
+Пример:
+while (условие) {
+    // do something, до тех пор пока условие истинно
+}
+
+*/
+
+import java.util.Scanner;
+
 public class F027_WhileLoops {
     public static void main(String[] args) {
+//        for (int i = 5; i > 0; i--)
+//            System.out.println("Hello World! " + i);
+        // перепишем код выше с использованием цикла while
+
+        int i = 5;
+        while (i > 0) {
+            System.out.println("Hello World! " + i);
+            i--;
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("stop")) {
+            System.out.print("Enter something or 'stop': ");
+            input = scanner.next().toLowerCase();
+            System.out.println("You entered: " + input);
+        }
+
+
 
     }
 }
