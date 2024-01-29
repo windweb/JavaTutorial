f957f5a3497543024a7fe58de118029e14295f07
Aidarov Kirill <github@aidarov.ru>
2024-01-29 15:27:04 +0300

    update F020_LogicalOperators

--- src/F020_LogicalOperators.java
+++ src/F020_LogicalOperators.java
@@ -1,5 +1,24 @@
+/* Logical Operators / Логические операторы
+        "||" - оператор OR
+        "&&" - оператор AND
+        "!" - оператор NOT
+*/
 public class F020_LogicalOperators {
     public static void main(String[] args) {
+        int temperature = 22;
+        boolean isWarm = temperature > 20 && temperature < 30;
+        System.out.println(isWarm);
+        // изменим temperature на 12
+        temperature = 12;
+        isWarm = temperature > 20 && temperature < 30;
+        System.out.println(isWarm);
 
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = ((hasHighIncome || hasGoodCredit) && !hasCriminalRecord);
+        // "||" - это оператор OR
+        // "&&" - оператор AND
+        System.out.println(isEligible);
     }
 }
